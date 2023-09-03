ff85b3be459f049d6c00692739add1abc81eb2de
Ryan N Wilson <ryan.nathan.wilson@gmail.com>
2023-09-02 23:15:57 -0400

    submit

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,7 @@ const config = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  printWidth: 80,
 };
 
 export default config;
--- src/components/block/Block.tsx
+++ src/components/block/Block.tsx
@@ -1,8 +1,48 @@
+'use client'
+import type { FocusEvent, KeyboardEvent } from 'react'
+import { useState } from 'react'
 import styles from './block.module.css'
+
 export default function Block() {
+  const [text, setText] = useState('')
+  const [editMode, setEditMode] = useState(true)
+
+
+  const handleSubmitField = (
+    e: KeyboardEvent<HTMLInputElement> | FocusEvent<HTMLInputElement>
+  ) => {
+    if (isKeyBoardEvent(e)) {
+      if (e.key === 'Enter') {
+        setEditMode(false)
+      }
+    } else if (e.type === 'blur') {
+      setEditMode(false)
+    }
+  }
+
   return (
-    <div className={styles.div}>
-      Some text
-    </div>
+    <>
+      {editMode ? (
+        <input
+          autoFocus
+          className={styles.block}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyUp={handleSubmitField}
+          onBlur={handleSubmitField}
+        />
+      ) : (
+        <div
+          className={styles.block}
+          onClick={() => setEditMode(true)}
+        >
+          {text}
+        </div>
+      )}
+    </>
   )
 }
+
+const isKeyBoardEvent = (x: any): x is KeyboardEvent => (
+  (x as KeyboardEvent).key !== undefined
+)
